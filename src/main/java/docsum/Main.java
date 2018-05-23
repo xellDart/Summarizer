4aee0b299bfa3e7fac660b446bfb99d3eb900286
xperiafan13-rom <mundorap2010@gmail.com>
2018-05-22 23:22:28 -0500

    CORS

--- src/main/java/docsum/Main.java
+++ src/main/java/docsum/Main.java
@@ -32,6 +32,28 @@ public class Main {
 
         Javalin app = Javalin.start(7000);
 
+        app.options("/*",
+                ctx -> {
+
+                    String accessControlRequestHeaders = ctx
+                            .header("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        ctx.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = ctx
+                            .header("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        ctx.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    ctx.status(200);
+                });
+
+        app.before(ctx -> ctx.header("Access-Control-Allow-Origin", "*"));
+
         app.post("/summarize", ctx -> {
             Data b64 = JsonIterator.deserialize(ctx.body(), Data.class);
             SummaryDeal summary = new SummaryDeal();
